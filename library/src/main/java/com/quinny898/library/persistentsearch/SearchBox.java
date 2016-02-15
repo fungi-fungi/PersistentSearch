7a52a8ae4e0a961c863d2a2560b624f391bd01cf
Iurii Tverezovskyi <Iurii Tverezovskyi>
2016-02-15 11:41:51 -0600

    Fixed issue with double click on back button

--- library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
+++ library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
@@ -944,11 +944,10 @@ public class SearchBox extends RelativeLayout {
 
 	@Override
 	public boolean dispatchKeyEvent(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.KEYCODE_BACK && getVisibility() == View.VISIBLE){
-			hideCircularly((Activity) getContext());
+		if(e.getKeyCode() == KeyEvent.KEYCODE_BACK && getSearchOpen()){
+			toggleSearch();
 			return true;
 		}
-
 		return super.dispatchKeyEvent(e);
 	}
 
