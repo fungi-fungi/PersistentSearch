a278692f19c79c54404c73ef16b7aa30e730e668
Iurii Tverezovskyi <Iurii Tverezovskyi>
2016-02-16 10:27:25 -0600

    Added clicable parameter to SearchResult

--- library/src/main/java/com/quinny898/library/persistentsearch/SearchResult.java
+++ library/src/main/java/com/quinny898/library/persistentsearch/SearchResult.java
@@ -6,16 +6,33 @@ public class SearchResult {
     public String title;
     public Drawable icon;
     public String value;
+    public boolean clickable;
 
     /**
-     * Create a search result with text and an icon
+     * Create a search result with text an icon and value
      * @param title
+     * @param value
      * @param icon
      */
     public SearchResult(String title, String value, Drawable icon) {
         this.title = title;
         this.icon = icon;
         this.value = value;
+        this.clickable = true;
+    }
+
+    /**
+     * Create a search result with clickable property
+     * @param title
+     * @param value
+     * @param icon
+     * @param clickable
+     */
+    public SearchResult(String title, String value, Drawable icon, boolean clickable) {
+        this.title = title;
+        this.icon = icon;
+        this.value = value;
+        this.clickable = clickable;
     }
 
     public int viewType = 0;
